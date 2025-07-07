1e9de74d5b2597356fa4fa95cb287f06da37f329
Anjansimhareddy <198r5a0514@gmail.com>
2025-07-07 18:38:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello from DevOps CI/CD!<br>welcome to my new project<br> Auto deplaoment success!This was updated code'));
+app.get('/', (req, res) => res.send('Hello from DevOps CI/CD!<br>welcome to my new project'));
 
 
 app.listen(port, () => console.log(`App running at http://localhost:${port}`));
